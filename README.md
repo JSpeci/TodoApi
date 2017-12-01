8fc11c2890e387358da2aae2a50147425ebda28e
Jan Specian <specian.j@st-software.com>
2017-12-01 13:35:12 +0100

    first commit - simple crud

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # TodoApi
 just testing something 
+
+
+Vyzkouset a pochopit DI - vlastni servis se statickým listem - nejaky logger
+
+docist getting started
---
+++ TodoApi/Startup.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using TodoApi.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TodoApi
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddDbContext<TodoContext>(opt => opt.UseInMemoryDatabase("TodoList"));
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app)
+        {
+            app.UseMvc();
+        }
+    }
+}
